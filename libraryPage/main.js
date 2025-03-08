4d2cc532d9706ef1c81687f3ca41aafb6a6d0920
Seung Hwang <2018066@naver.com>
2025-03-08 14:33:45 +0900

    feat / libraryList / 도서목록을 불러오는 getLibList 함수에 에러관리하는 코드들 추가. 기존 렌더링 하는 코드들을 try문에 넣어 관리. error가 발생했을 떄 화면에 보여줄  errorRender() 함수 추가

--- libraryPage/main.js
+++ libraryPage/main.js
@@ -3,6 +3,9 @@
 //2. 해당 도서를 보유한 리스트가 페이지에 나온다.
 //3. 리스트에 있는 도서관의 위치를 kakakoMap에 보내준다.
 
+const API_KEY =
+  "b14cca520846c95ce8b27fd7604c9de537395aba801650123783e2478da5a64c";
+
 const params = new URLSearchParams(window.location.search); //현재 URL의 쿼리 문자열을 분석하여 객체를 생성
 const isbn = params.get("isbn"); //쿼리 파라미터에서 isbn 값을 가져온다.
 
@@ -15,31 +18,52 @@ if (isbn) {
   );
 }
 
-const API_KEY =
-  "b14cca520846c95ce8b27fd7604c9de537395aba801650123783e2478da5a64c";
-
 let url1 = new URL( //도서검색api
-  `http://data4library.kr/api/srchBooks?authKey=${API_KEY}&keyword=어린왕자&pageNo=1&pageSize=10&format=json`
+  `http://data4library.kr/api/srchBooks?authKey=${API_KEY}&keyword=어린왕자&pageNo=1&pageSize=10&format=json`
 );
+
 let url2 = new URL( //도서 소장 도서관api
   `http://data4library.kr/api/libSrchByBook?authKey=${API_KEY}&region=11&isbn=9788995772423&format=json`
 );
 
 let libList = [];
 
-//api에서 데이터를 받아오는 함수
+//api에서 도서관 목록을 받아오는 함수
 const getLibList = async () => {
-  const response = await fetch(url2);
-  const data = await response.json();
-  console.log("data", data);
+  try {
+    const response = await fetch(url2);
+    const data = await response.json();
+    console.log("data", data);
 
-  libList = data.response.libs;
-  console.log("libList", libList);
+    if (response.status === 200) {
+      console.log("response.status", response.status);
+      if (!response.ok) {
+        throw new Error(`HTTP 오류 발생: ${response.status}`);
+      }
+      if (
+        !data.response ||
+        !data.response.libs ||
+        data.response.libs.length === 0
+      ) {
+        throw new Error("도서를 소장한 도서관이 없습니다.");
+      }
+      libList = data.response.libs;
+      console.log("libList", libList);
 
-  //   data.response.libs.forEach((item) => {
-  //     console.log("libList", item.lib);
-  //   });
-  libsRender();
+      //   data.response.libs.forEach((item) => {
+      //     console.log("libList", item.lib);
+      //   });
+      libsRender();
+    } else {
+      console.log("response.status", response.status);
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    if (error.message.includes("Failed to fetch")) {
+      errorRender("네트워크 오류 발생! 인터넷 연결을 확인해주세요.");
+    }
+    errorRender(error.message);
+  }
 };
 getLibList();
 
@@ -56,25 +80,29 @@ function libsRender() {
         <div>
           <dt class="col-lg-1" id="lib-call">
           <i class="fa-solid fa-phone"></i>전화번호</dt>
-          <dd class="col-lg-10"><a href="tel:${libs.lib.tel}">${libs.lib.tel}</a></dd>
+          <dd class="col-lg-10"><a href="tel:${libs.lib.tel}">${
+      libs.lib.tel || "정보 없음"
+    }</a></dd>
         </div>
         <div>
           <dt class="col-lg-1">
         <i class="fa-solid fa-location-dot"></i>
         주소</dt>
-          <dd class="col-lg-10" id="lib-address" onclick="copyAddress(event)">${libs.lib.address}</dd>
+          <dd class="col-lg-10" id="lib-address" onclick="copyAddress(event)">${
+            libs.lib.address || "주소 정보 없음"
+          }</dd>
         </div>
         <div>
           <dt class="col-lg-1" id="lib-time">
         <i class="fa-solid fa-clock"></i>
         영업시간</dt>
-          <dd class="col-lg-10">${libs.lib.operatingTime}</dd>
+          <dd class="col-lg-10">${libs.lib.operatingTime || "정보 없음"}</dd>
         </div>
         <div>
           <dt class="col-lg-1" id="lib-close-day">
         <i class="fa-solid fa-calendar-minus"></i>
         휴관일</dt>
-          <dd class="col-lg-10">${libs.lib.closed}</dd>
+          <dd class="col-lg-10">${libs.lib.closed || "정보 없음"}</dd>
         </div>
       </dl>
     </div>
@@ -83,6 +111,13 @@ function libsRender() {
   console.log("html :", libListHTML);
   document.getElementById("libs-board").innerHTML = libListHTML;
 }
+
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="col-lg-8" id="lib-name">
+  <i class="fa-solid fa-book"></i>${errorMessage}</div>
+  </div>`;
+  document.getElementById("libs-board").innerHTML = errorHTML;
+};
 
 function copyAddress(event) {
   //주소를 복사하는 함수
@@ -96,3 +131,16 @@ function copyAddress(event) {
       console.log("클립보드 복사에 실패했습니다.", err);
     });
 }
+
+//--------------------
+
+// var geocoder = new kakao.maps.services.Geocoder();
+
+// var callback = function (result, status) {
+//   if (status === kakao.maps.services.Status.OK) {
+//     console.log("지역 명칭 : " + result[0].address_name);
+//     console.log("행정구역 코드 : " + result[0].code);
+//   }
+// };
+
+// geocoder.coord2RegionCode(126.9786567, 37.566826, callback);
